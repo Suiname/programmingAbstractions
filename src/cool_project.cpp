df1a3f0739994bb71a854eb16f9d38aa4b2ce4bc
Jason Tham <jason.tham@tempus.com>
2017-12-08 00:24:05 -0600

    solved hw1 problem 2

--- src/cool_project.cpp
+++ src/cool_project.cpp
@@ -1,6 +1,7 @@
 // my project
 
 #include <iostream>
+#include <fstream>
 #include "console.h"
 #include "gwindow.h" // for GWindow
 #include "simpio.h"  // for getLine
@@ -33,11 +34,52 @@ void CensorString2(string &text, string remove) {
     }
 }
 
+struct scores
+{
+    int min = 0;
+    int max = 0;
+    float avg = 0;
+};
+
+int convertStringToInt(const std::string& input)
+{
+    std::stringstream ss(input);
+    int retval;
+    ss >> retval;
+    return retval;
+}
+
+scores getScores(string fileName){
+    scores result;
+    ifstream in(fileName.c_str());
+    int lineNumber = 0;
+    while (true) {
+        lineNumber++;
+        string line;
+        getLine(in, line);
+        if (in.eof()) break;
+        if (in.fail()) break;
+        int score;
+        score = convertStringToInt(line);
+        if ((score < result.min) || (lineNumber == 1)) {
+            result.min = score;
+        }
+        if (score > result.max) {
+            result.max = score;
+        }
+        result.avg = ((result.avg * (lineNumber - 1)) + score)/lineNumber;
+    }
+
+    return result;
+}
+
 int main() {
     string result = CensorString1("Stanford University", "nt");
     cout << result << endl;
     string result2 = "Llamas like to laugh";
     CensorString2(result2, "la");
     cout << result2 << endl;
+    scores result3 = getScores("scores.txt");
+    cout << "max: " << result3.max << " min: " << result3.min << " avg: " << result3.avg << endl;
     return 0;
 }
